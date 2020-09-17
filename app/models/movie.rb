4793668ffd724138ab2d967f1c9ab48b79c0cc32
Denise Henderson <denise@henderson3.us>
2020-09-17 13:53:37 +0000

    Automatically backed up by Learn

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,4 +5,10 @@ class Movie < ActiveRecord::Base
     binding.pry
   end
 
+  def can_be_instantiated_and_then_saved
+    movie = Movie.create(:title=>"This is a title.")
+    movie.title = "This is a title."
+    movie
+  end
+
 end
