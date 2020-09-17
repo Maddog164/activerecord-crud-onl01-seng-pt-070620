08d71e67857f6aa2bc57b15f1a7def4553041272
Denise Henderson <denise@henderson3.us>
2020-09-17 13:45:35 +0000

    Automatically backed up by Learn

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ActiveRecord::Base
 
-  Movie.create(:title=>"Inherent Vice",:release_date=>2014,:director=>"Paul Thomas Anderson",:lead=>"Joaquin Phoenix",:in_theaters=>true)
+  Movie.create(:title=>"This is a title")
 
 end
