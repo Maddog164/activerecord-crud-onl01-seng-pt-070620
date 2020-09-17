6b450ed93e6b7f9432629468ca581439e0aa92c9
Denise Henderson <denise@henderson3.us>
2020-09-17 14:14:33 +0000

    Automatically backed up by Learn

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -7,11 +7,16 @@ class Movie < ActiveRecord::Base
     movie.save
   end
 
-  # def can_be_instantiated_and_then_saved
-  #   movie = Movie.new
-  #   movie.title = "This is a title."
-  #   # binding.pry
-  #   movie.save
-  # end
+  def can_be_created_with_a_hash_of_attributes
+    # Initialize movie and then and save it
+    attributes = {
+        title: "The Sting",
+        release_date: 1973,
+        director: "George Roy Hill",
+        lead: "Paul Newman",
+        in_theaters: false
+    }
+    movie = Movie.create(attributes)
+  end
 
 end
